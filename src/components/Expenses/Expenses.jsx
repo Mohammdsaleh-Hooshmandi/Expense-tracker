d99def3f1d991a8d4480827a00ea4c62f81ea4de
Mohammdsaleh Hooshmandi <Mohammdsale.Hooshmandi1382@gmail.com>
2023-07-18 21:46:01 +0430

    add filter logic to Expenses

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
+
 import classes from "./Expenses.module.css";
 import ExpenseseList from "./ExpensesList";
+import ExpenseFilter from "./ExpensesFilter";
 
 function Expenses(props) {
+    const [filteredYear, setFilteredYear] = useState("2022");
+    const filteredExpenses = props.items.filter(expense => expense.date.getFullYear().toString() === filteredYear)
+
+    function filterChangeHandler (selectedYear) {
+        setFilteredYear(selectedYear);
+        console.log(filteredYear)
+    }
     return (
         <div className={classes.expenses}>
-            <ExpenseseList items={props.items} />
+            <ExpenseFilter selectedYear={filteredYear} onChangeFilter={filterChangeHandler} />
+            <ExpenseseList items={filteredExpenses} />
         </div>
     );
 }
