34859686039e062821eba20b5871b66f98d11da5
Mohammdsaleh Hooshmandi <Mohammdsale.Hooshmandi1382@gmail.com>
2023-07-18 17:48:37 +0430

    create Expenses

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import NewExpense from "./components/NewExpense/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
 
 function App() {
 	return(
 		<>
 			<NewExpense />
+			<Expenses />
 		</>
 	);
 }
---
+++ src/components/Expenses/Expenses.jsx
@@ -0,0 +1,9 @@
+import classes from "./Expenses.module.css";
+
+function Expenses (props) {
+    return (
+        <div className={classes.expenses}></div>
+    );
+}
+
+export default Expenses;
